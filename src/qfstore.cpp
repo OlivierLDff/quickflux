d9abc69d1544bca0f45a3231946f8e790f2febc1
Olivier Le Doeuff <olivier.ldff@gmail.com>
2024-09-18 14:24:17 +0000

    ⬆️ Qt6
    PR is based on https://github.com/OneIdentity/quickflux/pull/1 by Imre Steiner
    
    Co-authored-by: Imre Steiner <imresteiner@users.noreply.github.com>

--- src/qfstore.cpp
+++ src/qfstore.cpp
@@ -117,15 +117,15 @@ Store {
 The default value is false
  */
 
-
-QFStore::QFStore(QObject *parent) : QObject(parent) , m_filterFunctionEnabled(false)
+QFStore::QFStore(QObject* parent)
+    : QObject(parent)
+    , m_filterFunctionEnabled(false)
 {
-
 }
 
 QQmlListProperty<QObject> QFStore::children()
 {
-    return QQmlListProperty<QObject>(qobject_cast<QObject*>(this), m_children);
+    return QQmlListProperty<QObject>(qobject_cast<QObject*>(this), &m_children);
 }
 
 void QFStore::dispatch(QString type, QJSValue message)
@@ -133,38 +133,45 @@ void QFStore::dispatch(QString type, QJSValue message)
     QQmlEngine* engine = qmlEngine(this);
     QF_PRECHECK_DISPATCH(engine, type, message);
 
-    foreach(QObject* child , m_children) {
+    foreach(QObject* child, m_children)
+    {
         QFStore* store = qobject_cast<QFStore*>(child);
-        if (!store) {
+        if(!store)
+        {
             continue;
         }
         store->dispatch(type, message);
     }
 
-    foreach(QObject* child , m_redispatchTargets) {
+    foreach(QObject* child, m_redispatchTargets)
+    {
         QFStore* store = qobject_cast<QFStore*>(child);
 
-        if (!store) {
+        if(!store)
+        {
             continue;
         }
         store->dispatch(type, message);
     }
 
-    if (m_filterFunctionEnabled) {
-        const QMetaObject *meta = metaObject();
+    if(m_filterFunctionEnabled)
+    {
+        const QMetaObject* meta = metaObject();
         QByteArray signature;
         int index;
 
         signature = QMetaObject::normalizedSignature(QString("%1(QVariant)").arg(type).toUtf8().constData());
-        if ( (index = meta->indexOfMethod(signature.constData())) >= 0 ) {
+        if((index = meta->indexOfMethod(signature.constData())) >= 0)
+        {
             QMetaMethod method = meta->method(index);
             QVariant value = QVariant::fromValue<QJSValue>(message);
 
-            method.invoke(this,Qt::DirectConnection, Q_ARG(QVariant, value));
+            method.invoke(this, Qt::DirectConnection, Q_ARG(QVariant, value));
         }
 
         signature = QMetaObject::normalizedSignature(QString("%1()").arg(type).toUtf8().constData());
-        if ( (index = meta->indexOfMethod(signature.constData())) >= 0 ) {
+        if((index = meta->indexOfMethod(signature.constData())) >= 0)
+        {
             QMetaMethod method = meta->method(index);
 
             method.invoke(this);
@@ -174,53 +181,55 @@ void QFStore::dispatch(QString type, QJSValue message)
     emit dispatched(type, message);
 }
 
-void QFStore::bind(QObject *source)
+void QFStore::bind(QObject* source)
 {
     setBindSource(source);
 }
 
 void QFStore::setup()
 {
-    QFActionCreator *creator = 0;
+    QFActionCreator* creator = 0;
     QFDispatcher* dispatcher = 0;
 
     creator = qobject_cast<QFActionCreator*>(m_bindSource.data());
 
-    if (creator) {
+    if(creator)
+    {
         dispatcher = creator->dispatcher();
-    } else {
+    }
+    else
+    {
         dispatcher = qobject_cast<QFDispatcher*>(m_bindSource.data());
     }
 
-    if (m_actionCreator.data() == creator &&
-        m_dispatcher.data() == dispatcher) {
+    if(m_actionCreator.data() == creator && m_dispatcher.data() == dispatcher)
+    {
         // Nothing changed.
         return;
     }
 
-    if (!m_actionCreator.isNull() &&
-        m_actionCreator.data() != creator) {
+    if(!m_actionCreator.isNull() && m_actionCreator.data() != creator)
+    {
         m_actionCreator->disconnect(this);
     }
 
-    if (!m_dispatcher.isNull() &&
-        m_dispatcher.data() != dispatcher) {
+    if(!m_dispatcher.isNull() && m_dispatcher.data() != dispatcher)
+    {
         m_dispatcher->disconnect(this);
     }
 
     m_actionCreator = creator;
     m_dispatcher = dispatcher;
 
-    if (!m_actionCreator.isNull()) {
-        connect(m_actionCreator.data(),SIGNAL(dispatcherChanged()),
-                this,SLOT(setup()));
+    if(!m_actionCreator.isNull())
+    {
+        connect(m_actionCreator.data(), SIGNAL(dispatcherChanged()), this, SLOT(setup()));
     }
 
-    if (!m_dispatcher.isNull()) {
-        connect(dispatcher,SIGNAL(dispatched(QString,QJSValue)),
-                this,SLOT(dispatch(QString,QJSValue)));
+    if(!m_dispatcher.isNull())
+    {
+        connect(dispatcher, SIGNAL(dispatched(QString, QJSValue)), this, SLOT(dispatch(QString, QJSValue)));
     }
-
 }
 
 /*! \qmlproperty array Store::redispatchTargets
@@ -245,9 +254,8 @@ void QFStore::setup()
 
 QQmlListProperty<QObject> QFStore::redispatchTargets()
 {
-    return QQmlListProperty<QObject>(qobject_cast<QObject*>(this), m_redispatchTargets);
+    return QQmlListProperty<QObject>(qobject_cast<QObject*>(this), &m_redispatchTargets);
 }
-
 
 /*! \qmlproperty object Store::bindSource
  *
@@ -256,13 +264,12 @@ QQmlListProperty<QObject> QFStore::redispatchTargets()
  * The default value is null, and it listens to AppDispatcher
  */
 
-
-QObject *QFStore::bindSource() const
+QObject* QFStore::bindSource() const
 {
     return m_bindSource.data();
 }
 
-void QFStore::setBindSource(QObject *source)
+void QFStore::setBindSource(QObject* source)
 {
     m_bindSource = source;
     setup();
